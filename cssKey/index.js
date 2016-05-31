5c82e19a201330582c714f88c7df5da361e2c5d3
Harry Keller <ich@harry-k.de>
2016-05-31 10:43:02 +0200

    clean up some whitespace

--- cssKey/index.js
+++ cssKey/index.js
@@ -14,9 +14,6 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-
-
-
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
